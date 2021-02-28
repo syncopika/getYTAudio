3eef56af3412731316beebb98f14f21073d15157
nicholas hung <nhung93@outlook.com>
2021-02-28 14:29:02 -0500

    got the parse_weba script in a better place now :). added a readme.

--- parse_weba_file/parse_weba.js
+++ parse_weba_file/parse_weba.js
@@ -7,6 +7,7 @@
 // https://www.darkcoding.net/software/reading-mediarecorders-webm-opus-output/
 // https://www.matroska.org/technical/diagram.html
 // https://www.matroska.org/technical/specs/index.html
+// https://www.matroska.org/technical/ordering.html
 // weba files are actually webm files! so since not matroska file, we look at doctype field (0x81 0x04 for webm)
 
 // https://www.webmproject.org/docs/container/
@@ -31,20 +32,31 @@ const EBMLElements = {
 // the keys and all the children keys are ebml elements!
 // so: id, length, value is the order of info
 // note that length might be 2 bytes and not just 1, depending on the first byte
+// I'm using the id of each element as the key for easy look up since there's so mandatory ordering of most of the elements
 const EBMLTopLevelElements = {
 	"114d9b74": {
 		"name": "SeekHead",
 		"children": {
-			"Seek": "4dbb",
-			"SeekID": "53ab",
-			"SeekPosition": "53ac"
+			"4dbb": {
+				"name": "Seek"
+			},
+			"53ab": {
+				"name": "SeekID"
+			},
+			"53ac": {
+				"name": "SeekPosition"
+			}
 		}
 	},
 	"1549a966": {
 		"name": "Info",
 		"children": {
-			"SegmentUID": "73a4",
-			"SegmentFilename": "7384"
+			"73a4": {
+				"name": "SegmentUID"
+			},
+			"7384": {
+				"name": "SegmentFilename"
+			}
 		}
 	},
 	"1654ae6b": {
@@ -55,15 +67,27 @@ const EBMLTopLevelElements = {
 		"name": "Chapters",
 		"children": {}
 	},
-	"1f4b675": {
+	"1f43b675": {
 		"name": "Cluster",
 		"children": {
-			"Timestamp": "e7",
-			"SilentTracks": "5854",
-			"Position": "a7",
-			"PrevSize": "ab",
-			"SimpleBlock": "a3",
-			"BlockGroup": "a0"
+			"e7": {
+				"name": "Timestamp"
+			},
+			"5854": {
+				"name": "SilentTracks"
+			},
+			"a7": {
+				"name": "Position"
+			},
+			"ab": {
+				"name": "PrevSize"
+			},
+			"a3": {
+				"name": "SimpleBlock"
+			},
+			"a0": {
+				"name": "BlockGroup"
+			}
 		}
 	},
 	"1c53bb6b": {
@@ -184,51 +208,32 @@ function additionalOctetCount(binStr){
 
 function getElement(buffer, start, end, topLevelElements, info){
 	let bufferSlice = buffer.slice(start, end);
-	console.log(bufferSlice);
 	if(topLevelElements[bufferSlice.toString('hex')] !== undefined){
 		//console.log(varIntBinToDec(hexToBin(getElementLength(end, buffer).toString('hex'))));
 		const topLevelElementName = topLevelElements[bufferSlice.toString('hex')].name;
-		info[topLevelElementName] = {
+		
+		const newElement = {
 			"id": ("0x" + bufferSlice.toString('hex')),
+			
+			// sometimes the length could be 01 ff ff ff ff ff ff ff, which is unknown length
+			// TODO: maybe say unknown length instead of converting to decimal?
 			"length": varIntBinToDec(hexToBin(getElementLength(end, buffer).toString('hex'))), // length of an element's content
+			
 			"lengthNumBytes": getElementLength(end, buffer).length, // the number of bytes to represent the length
+			"position": start,
+		};
+		
+		if(info[topLevelElementName]){
+			// some elements might show up multiple times like simpleblock. so just append to a list if we have multiple.
+			info[topLevelElementName].push(newElement);
+		}else{
+			info[topLevelElementName] = [newElement];
 		}
-		return info[topLevelElementName];
+		return newElement;
 	}else{
 		return null;
 	}
 }
-
-/* oops, already wrote this function in a much better way a while back below lol -__-
-function getElementLength(buffer, start, end){
-	// read one byte to figure out the length
-	// if the byte as binary has a leading 0, need to read one more byte
-	// to get the length
-	// we're assuming the start and end indices of the buffer 
-	// are right after the element id
-	let firstByte = buffer.slice(start, end);
-	let byteBin = hexToBin(firstByte.toString('hex'));
-	
-	// check how many leading 0s. that'll determine how many more bytes to be
-	// read for calculating the length of the ebml element
-	if(byteBin[0] === "1"){
-		// length is just firstByte
-		return binToDec(byteBin);
-	}else{
-		let newBinStr = byteBin;
-		let numMoreBytes = byteBin.indexOf("1"); // just get index of first appearance of 1. this equals the number of leading 0s.
-		for(let i = end; i < end + numMoreBytes; i++){
-			let s = i;   // new start
-			let e = i+1; // new end
-			let newBin = hexToBin(buffer.slice(s, e).toString('hex'));
-			newBinStr += newBin;
-		}
-		// calculate the length of the ebml element with the new binary string
-		// need to throwaway the leading 1
-		newBinStr = newBinStr.replace("1", "0");
-		return binToDec(newBinStr);
-	}
-}*/
 
 // pass pos of first byte of length for an element
 // gets a slice of the buffer that represents all the bytes that represent
@@ -269,11 +274,6 @@ function parseWebm(buffer){
 	info["doctype_length"] = buffer.slice(23,24);
 	info["doctype_value"] = buffer.slice(24,28); // this is bad, but assume 4 bytes
 	
-	/*
-	for(let key in info){
-		info[key] = "0x" + info[key].toString('hex');
-	}*/
-	
 	// now we get into the segment
 	let nextPos = 36; // 36 because 4 + 1 + 31 for the EBML header.
 	info["segment"] = buffer.slice(nextPos, nextPos+4);
@@ -281,13 +281,12 @@ function parseWebm(buffer){
 	
 	// note that the length here is the number of bytes that make up the length of the element (not the actual total length of the element itself)
 	// there isn't an additional number to add (i.e. the acutal length of the element) in this case other than the 4 bytes for the header and the num of bytes
-	// that represent the element's length because this is the segment element, which contains the top-level elements.
+	// that represent the element's length because this is the segment element, which contains the top-level elements. 4 is ok here for top-level elements
+	// but some lower-level element headers may not be 4 bytes!
 	let nextElementPos = nextPos + 4 + info["segment_length"].length;
 	let nextElement = getElement(buffer, nextElementPos, nextElementPos+4, EBMLTopLevelElements, info);
 	
-	for(let i = 0; i < 4; i++){
-		console.log(nextElement);
-		console.log(nextElementPos);
+	for(let i = 0; i < 4; i++){ // change to while loop? using 4 just seemed like a good limit for testing
 		if(nextElement === null){
 			break;
 		}
@@ -295,173 +294,6 @@ function parseWebm(buffer){
 		nextElement = getElement(buffer, nextElementPos, nextElementPos+4, EBMLTopLevelElements, info);
 	}
 	
-	return info;
-}
-
-// returns ebml info in a dictionary
-function readEBMLInfo(buffer){
-	// first 4 bytes = signify it's an ebml element 
-	// next byte is the length
-	// next 2 bytes is ebml version
-	// next byte is value of this element 
-	let info = {};
-	info["header"] = buffer.slice(0,4); //1a45dfa3
-	info["headerLength"] = buffer.slice(4,5); // 9f
-	info["version_element"] = buffer.slice(5,7); // 4286
-	info["length"] = buffer.slice(7,8); // 81
-	info["value"] = buffer.slice(8,9); // 01
-	
-	info["readversion_element"] = buffer.slice(9,11);
-	info["readversion_length"] = buffer.slice(11,12);
-	info["readversion_value"] = buffer.slice(12,13);
-	
-	info["maxid_element"] = buffer.slice(13,15);
-	info["maxid_length"] = buffer.slice(15,16);
-	info["maxid_value"] = buffer.slice(16,17);
-	
-	info["maxsize_element"] = buffer.slice(17,19);
-	info["maxsize_length"] = buffer.slice(19,20);
-	info["maxsize_value"] = buffer.slice(20,21);
-	
-	info["doctype_element"] = buffer.slice(21,23);
-	info["doctype_length"] = buffer.slice(23,24);
-	info["doctype_value"] = buffer.slice(24,28); // this is bad, but assume 4 bytes 
-	
-	// as soon as we know the length of the header, we can move past it.
-	// we just need to verify webm 
-	// then get the audio info 
-	let nextPos = 36; // 36 because 4 + 1 + 31 for the EBML header.
-	info["segment"] = buffer.slice(nextPos, nextPos+4);
-	info["segment_length"] = getElementLength(nextPos+4, buffer);
-	
-	// should be at segment. look for segment elements
-	// note that there might not be a seekhead! the element is recommended but not mandatory
-	let pos = nextPos+4+info["segment_length"].length;
-	info["seekhead"] = buffer.slice(pos, pos+4);
-	info["seekhead_length"] = getElementLength(pos+4, buffer);
-	
-	// now we got all the octets (or bytes) for the length.
-	// we need to convert to binary and truncate as needed to calculate the integer value.
-	let len = varIntBinToDec(hexToBin(info["seekhead_length"]));
-	//console.log("length to skip: " + len);
-	
-	let newPos = pos+4+len+1;
-	info["info_element"] = buffer.slice(newPos, newPos+4);
-	info["info_element_length"] = getElementLength(newPos+4, buffer);
-	let len2 = varIntBinToDec(hexToBin(info["info_element_length"]));
-	//console.log("length to skip: " + len2);
-	
-	newPos = newPos+4+len2+1;
-	info["tracks_element"] = buffer.slice(newPos, newPos+4);
-	info["tracks_element_length"] = getElementLength(newPos+4, buffer);
-	//let len3 = varIntBinToDec(hexToBin(info["tracks_element_length"]));
-	
-	// since tracks is a level 1 element, its value after the length encompasses all its 
-	// underlying elements
-	// so the next byte after the length is a new element 
-	newPos = newPos+4+1;
-	info["track_entry_element"] = buffer.slice(newPos, newPos+1);
-	info["track_entry_element_length"] = getElementLength(newPos+1, buffer);
-	//let len3 = varIntBinToDec(hexToBin(info["tracks_element_length"]));
-	
-	// next is tracknumber 
-	newPos += 2;
-	info["tracknumber"] = buffer.slice(newPos, newPos+1);
-	info["tracknumber_length"] = getElementLength(newPos+1, buffer);
-	
-	newPos += 1 + varIntBinToDec(hexToBin(info["tracknumber_length"])) + 1;
-	info["trackUID"] = buffer.slice(newPos, newPos+2);
-	info["trackUID_length"] = getElementLength(newPos+2, buffer);
-	
-	newPos += 2 + varIntBinToDec(hexToBin(info["trackUID_length"])) + 1;
-	info["tracktype"] = buffer.slice(newPos, newPos+1);
-	info["tracktype_length"] = getElementLength(newPos+1, buffer);
-	
-	newPos += 1 + varIntBinToDec(hexToBin(info["tracktype_length"])) + 1;
-	info["flaglacing"] = buffer.slice(newPos, newPos+1);
-	info["flaglacing_length"] = getElementLength(newPos+1, buffer);
-	
-	newPos += 1 + varIntBinToDec(hexToBin(info["flaglacing_length"])) + 1;
-	info["codecid"] = buffer.slice(newPos, newPos+1);
-	info["codecid_length"] = getElementLength(newPos+1, buffer);
-	let codecIdLen = varIntBinToDec(hexToBin(info["codecid_length"]));
-	info["codecid_value"] = buffer.slice(
-		newPos+info["codecid_length"].length+1, 
-		newPos+1+info["codecid_length"].length+codecIdLen).toString();
-	
-	// after confirming codec id as opus, find the data 
-	// codec-specific info here 
-	newPos = newPos+1+info["codecid_length"].length+codecIdLen;
-	info["privatecodecinfo"] = buffer.slice(newPos, newPos+2);
-	info["privatecodecinfo_length"] = getElementLength(newPos+2, buffer);
-	
-	// skip over private info 
-	//console.log("skip over: " + varIntBinToDec(hexToBin(info["privatecodecinfo_length"])) + " bytes.");
-	newPos += varIntBinToDec(hexToBin(info["privatecodecinfo_length"])) + 3;
-	info["codecdelay"] = buffer.slice(newPos, newPos+2);
-	info["codecdelay_length"] = getElementLength(newPos+2, buffer);
-	
-	// skip [56][aa] and [56][bb] 
-	newPos += varIntBinToDec(hexToBin(info["codecdelay_length"])) + 3;
-	info["seekpreroll"] = buffer.slice(newPos, newPos+2);
-	info["seekpreroll_length"] = getElementLength(newPos+2, buffer);
-	
-	// find [e1] for audio metadata 
-	newPos += varIntBinToDec(hexToBin(info["seekpreroll_length"])) + 3;
-	info["audio"] = buffer.slice(newPos, newPos+1);
-	info["audio_length"] = getElementLength(newPos+1, buffer);
-	
-	newPos += 1 + info["audio_length"].length;
-	info["samplingFrequency"] = buffer.slice(newPos, newPos+1);
-	info["samplingFrequency_length"] = getElementLength(newPos+1, buffer);
-	let sampleFreqLen = varIntBinToDec(hexToBin(info["samplingFrequency_length"]));
-	
-	info["samplingFrequency_value"] = buffer.slice(
-		newPos+1+info["samplingFrequency_length"].length,
-		newPos+1+info["samplingFrequency_length"].length+sampleFreqLen
-	).toString();
-	
-	newPos = newPos+1+info["samplingFrequency_length"].length+sampleFreqLen;
-	info["channels"] = buffer.slice(newPos, newPos+1);
-	info["channels_length"] = getElementLength(newPos+1, buffer);
-	let channelLen = varIntBinToDec(hexToBin(info["channels_length"]));
-	info["channels_value"] = buffer.slice(
-		newPos+1+info["channels_length"].length,
-		newPos+1+info["channels_length"].length+channelLen
-	);
-	
-	/* channels should just be converted straight to decimal from its binary I think?
-	console.log(
-	binToDec(hexToBin(
-		buffer.slice(
-			newPos+1+info["channels_length"].length,
-			newPos+1+info["channels_length"].length+channelLen
-			)
-	)));*/
-		
-	newPos = newPos+1+info["channels_length"].length+channelLen;
-	info["bitdepth"] = buffer.slice(newPos, newPos+2);
-	info["bitdepth_length"] = getElementLength(newPos+2, buffer);
-	let bitdepthLen = varIntBinToDec(hexToBin(info["bitdepth_length"]));
-	
-	// next is the Cueing Data section 
-	newPos = newPos+2+info["bitdepth_length"].length+bitdepthLen;
-	info["cueingData"] = buffer.slice(newPos, newPos+4);
-	info["cueingData_length"] = getElementLength(newPos+4, buffer);
-	let cueingDataLength = varIntBinToDec(hexToBin(info["cueingData_length"]));
-	//console.log(cueingDataLength);
-	
-	// find simpleblock for data 
-	newPos = newPos + 4 + info["cueingData_length"].length + cueingDataLength;
-	info["cluster"] = buffer.slice(newPos, newPos+4);
-	info["cluster_length"] = getElementLength(newPos+4, buffer);
-	console.log(varIntBinToDec(hexToBin(info["cluster_length"])));
-	
-	// can reach simpleblock now (look for A3)
-	// dunno what to do with the data once I get it though?
-	// I think ogg containers can hold opus-encoded audio data, but I don't think 
-	// I can just stick this data in an ogg container, but even if I could I'd have to 
-	// create the container first?
 	return info;
 }
 
@@ -478,10 +310,21 @@ fs.open(theFile, 'r', (status, fd) => {
 	let ebml = parseWebm(buffer);
 	console.log(ebml);
 	
-	//let ebml = readEBMLInfo(buffer);
-	//console.log(ebml);
+	// let's take a look in the cluster element (that's where our audio data should be)
+	let cluster = {};
 	
-	// TODO: readEBMLInfo is messed up because the top level elements could be in any order!
-	// used the top level elements object to figure out what the current top level element is.
+	// the divide by 2 stuff is for calculating how many bytes the element's header is
+	let startPos = ebml.Cluster[0].position + ebml.Cluster[0].lengthNumBytes + ((ebml.Cluster[0].id.length-2)/2);
 	
+	// based on the matroska spec, the timestamp has to be first in the cluster, so we can assume it'll be just 1 byte (hence the +1)
+	let nextElement = getElement(buffer, startPos, startPos+1, EBMLTopLevelElements["1f43b675"].children, cluster); 
+	
+	while(nextElement != null){
+		startPos += (nextElement.length + nextElement.lengthNumBytes + ((nextElement.id.length-2)/2));
+		nextElement = getElement(buffer, startPos, startPos+1, EBMLTopLevelElements["1f43b675"].children, cluster);
+	}
+	
+	console.log("=====================");
+	// show just elements within cluster (we want to see a lot of simpleblock elements cause that's our audio data)
+	console.log(cluster);
 });
